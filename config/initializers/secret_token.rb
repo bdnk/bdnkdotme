9e989458b1ed52f0c0ef5d42e48671704d9e6f5e
bdnk <brendanjkeyworth@gmail.com>
2015-01-31 12:04:48 -0500

    Initial Commit
    This is a landing page for the bdnk webpage

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BdnkGithub::Application.config.secret_key_base = 'a9f351d48f92291f7d87e718e0534a248b586e112000bfd86ee6bd80d876876da1cabac76712d0d37fe1691226e268ddc66866f7eef649fe65eebb70f9b42c97'
